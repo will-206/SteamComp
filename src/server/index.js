657e26f35a3ce4c1a99a2b54a625d888c6c3b391
will-206 <will9_30@yahoo.com>
2017-03-22 14:24:49 -0700

    heroku options test3

--- src/server/index.js
+++ src/server/index.js
@@ -33,7 +33,7 @@ passport.deserializeUser(function(obj, done) {
 
 passport.use(new SteamStrategy({
     returnURL: `https://steamcomp.herokuapp.com/api/auth/steam/return`,
-    realm: `http://localhost:${WEB_PORT}/`,
+    realm: `https://steamcomp.herokuapp.com:${WEB_PORT}/`,
     apiKey: process.env.STEAM_API_KEY
   },
   function(identifier, profile, done) {
